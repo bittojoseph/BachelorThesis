c192d5822b3f9f4b89ca52b2f62a0f4ae9121d95
Addythia Saphala <addythia@gmail.com>
2020-11-18 13:56:36 +0100

    Updated codes

--- Codes/readSerial.py
+++ Codes/readSerial.py
@@ -2,12 +2,17 @@ import serial
 import csv
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
-#with open("/home/bitto/Documents/GIT/Bachelor_Thesis/Codes/Calibration_data.csv", 'w', newline='') as csvfile:
+with open('/home/student/Documents/Bitto/BachelorThesis/Codes/calibration_file.csv', mode='w') as calibration_file:
+    calibration_writer = csv.writer(calibration_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    calibration_writer.writerow(['Acceleration in X', 'Acceleration in Y', 'Acceleration in Z'])
+calibration_list = []
 while True:
     try:
         ser_bytes = ser.readline()
-        decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode("utf-8")
-        print(decoded_bytes + '\n')
+        #decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode("utf-8")
+        #calibration_list = decoded_bytes.split(" ")
+        print(ser_bytes + '\n')
     except:
         print("Keyboard Interrupt")
         break
